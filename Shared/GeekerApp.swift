f7e1d7dd412108bc0fa510da1a71005b224a9423
Anthony Okpako <melowsworld@yahoo.co.uk>
2021-12-12 00:45:31 +0100

    Initial Commit

---
+++ Shared/GeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeekerApp.swift
+//  Shared
+//
+//  Created by Tony on 12/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct GeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
